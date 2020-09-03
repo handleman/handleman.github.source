86a526f04e5b328973befaa1f8a240ee0f2fe7d8
handleman <pavel.handleman@gmail.com>
2020-09-03 12:27:43 +0300

    like button

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,5 +1,7 @@
 (function (global) {
 	'use strict';
+	//TODO: 1. change controller, view, state to Classes with construtor initialization.
+	//TODO: 2.  move constants to proper classes
 	const containerEl = document.getElementById('container');
 	containerEl.style.minHeight = `${window.innerHeight}px`;
 
@@ -9,6 +11,8 @@
 
 	const visit_webhook = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/handleman_github_io-ybwyw/service/log_visit/incoming_webhook/visit';
 	const visits_webhook = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/handleman_github_io-ybwyw/service/log_visit/incoming_webhook/get_visits'
+	const put_like_webhook = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/handleman_github_io-ybwyw/service/set_like/incoming_webhook/like';
+	const get_likes_webhook = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/handleman_github_io-ybwyw/service/set_like/incoming_webhook/get_likes';
 	const geo_lookup = 'https://json.geoiplookup.io/';
 
 	const pathnameParts = window.location.pathname.split('/');
@@ -18,22 +22,45 @@
 
 	const excludedHosts = new Set(['127.0.0.1', 'localhost']);
 	const hostname = window.location.hostname;
+	const os = window.navigator.platform;
 
 	const state = {
 		article,
-		hostname
+		hostname,
+		userGeoData: {},
+		os,
 	};
 
 	const controller = {
 
-		approveVisit: (userData, article) => {
+		approveVisit: (article, ip, country_name) => {
 
 			const date = new Date().toUTCString();
 			console.log('article is', article);
-
-			fetch(`${visit_webhook}?article=${article}&ip=${userData.ip}&country_name=${userData.country_name}&date=${date}`)
+			fetch(`${visit_webhook}?article=${article}&ip=${ip}&country_name=${country_name}&date=${date}`)
 				.then((res) => {
-					res.ok ? console.log('visit approved') : console.error('visit couldn be accepted!');
+					res.ok ? console.log('visit approved') : console.error('visit couldnt be accepted!');
+				});
+		},
+
+		approveLike: (article, ip = null, os) => {
+			const date = new Date().toUTCString() || null;
+			const requestOptions = {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					article,
+					ip,
+					os,
+					date
+				})
+			}
+
+			fetch(put_like_webhook, requestOptions)
+				.then((res) => {
+					res.ok ? console.log('like approved') : console.error('like couldnt be accepted!');
 				});
 		},
 
@@ -48,11 +75,33 @@
 	const view = {
 
 		viewsSelectorID: 'views-number',
+		likesSelectorID: 'like-button',
+		likesActiveClassName: 'liked',
 		updateViews: (viewsNumber) => {
 			const id = view.viewsSelectorID;
 			const el = document.getElementById(id);
 			if (el) {
 				el.textContent = viewsNumber;
+			}
+		},
+		likeButtonClickEvent: (callback) => {
+			const id = view.likesSelectorID;
+			const el = document.getElementById(id);
+			const activeClassName = view.likesActiveClassName;
+			el.onclick = () => {
+				if (el && !el.classList.contains(activeClassName)) {
+					callback()
+					view.likeButtonSetActive()
+				}
+			}
+		},
+		likeButtonSetActive() {
+			const id = view.likesSelectorID;
+			const el = document.getElementById(id);
+			const activeClassName = view.likesActiveClassName;
+
+			if (el && !el.classList.contains(activeClassName)) {
+				el.classList.add(activeClassName);
 			}
 		}
 	};
@@ -60,13 +109,16 @@
 	controller.getVisits(state.article)
 		.then(data => view.updateViews(data));
 
+	view.likeButtonClickEvent(() => controller.approveLike(state.article, state.userGeoData.ip, state.os));
+
 	if (!excludedHosts.has(state.hostname)) {
 
 
 		setTimeout(() => {
 			fetch(geo_lookup)
 				.then(response => response.json())
-				.then(userData => controller.approveVisit(userData, state.article));
+				.then(fetchData => state.userGeoData = fetchData)
+				.then(() => controller.approveVisit(state.article, state.userGeoData.ip, state.userGeoData.country_name));
 		}, 5000);
 	}
 
